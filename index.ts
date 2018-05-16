2cb1b785b7b3f2d54726b8e061cf4ee0596e756d
DESKTOP-F22QFVU\hungt <hung.tovi@gmail.com>
2018-05-16 21:57:26 +0700

    Add router module

--- index.ts
+++ index.ts
@@ -1,12 +1,15 @@
 import { NgModule, ModuleWithProviders, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppInsightsConfig, AppInsightsService } from './src/app-insight.service';
 
 export * from './src/app-insight.service';
 
+const routes: Routes = [];
+
 @NgModule({
-  imports: [ CommonModule ],
+  imports: [ CommonModule, RouterModule.forRoot() ],
   declarations: [],
   exports: [],
   providers: [ AppInsightsService ]
